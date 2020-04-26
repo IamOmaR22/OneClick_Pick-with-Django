a07f21238f98421d2b403f3a167d595968fe8ddf
Md. Omar Faruk <iamomar022@gmail.com>
2020-04-26 11:02:11 +0600

    Product Models

--- shop/models.py
+++ shop/models.py
@@ -4,8 +4,12 @@ from django.db import models
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, default="")
+    subcategory = models.CharField(max_length=50, default="")
+    price = models.IntegerField(default=0)
     description = models.CharField(max_length=500)
     pub_date = models.DateField()
+    image = models.ImageField(upload_to="shop/images", default="")
 
     def __str__(self):
         return self.product_name
