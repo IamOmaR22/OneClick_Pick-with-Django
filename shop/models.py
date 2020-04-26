3b14a7cecc56c8845d331fe776d5cd8f66b4fc34
Md. Omar Faruk <iamomar022@gmail.com>
2020-04-26 10:48:08 +0600

    Shop Home Page Design

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    product_id = models.AutoField
+    product_name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    pub_date = models.DateField()
+
+    def __str__(self):
+        return self.product_name
