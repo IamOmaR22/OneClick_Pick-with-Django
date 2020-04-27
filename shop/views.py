69469ddc49ac6fcf472d61a1e3e97d84e31327df
Md. Omar Faruk <iamomar022@gmail.com>
2020-04-27 14:22:28 +0600

    Category Wise Product Display

--- shop/views.py
+++ shop/views.py
@@ -6,18 +6,26 @@ from math import ceil
 # Create your views here.
 
 def index(request):
-    products = Product.objects.all()
+    # products = Product.objects.all()
 
-    n = len(products)  # length of products
-    nSlides = n//4 + ceil((n/4)-(n//4))   # Number of slides
+    # n = len(products)  # length of products
+    # nSlides = n//4 + ceil((n/4)-(n//4))   # Number of slides
 
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)  # length of products
+        nSlides = n//4 + ceil((n/4)-(n//4))   # Number of slides
+        allProds.append([prod, range(1, nSlides), nSlides])
     # context = {'products':products, 'no_of_slides':nSlides, 'range':range(1, nSlides)}
-    all_products = [
-        [products, range(1, nSlides), nSlides],
-        [products, range(1, nSlides), nSlides]
-    ]
+    # all_products = [
+    #     [products, range(1, nSlides), nSlides],
+    #     [products, range(1, nSlides), nSlides]
+    # ]
 
-    context = {'all_products':all_products}
+    context = {'all_products':allProds}
     return render(request, 'shop/index.html', context)
 
 def about(request):
