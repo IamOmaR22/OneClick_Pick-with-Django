ad06f7edf1938a782edafd067c8ed077500ea1c6
Md. Omar Faruk <iamomar022@gmail.com>
2020-04-28 10:52:53 +0600

    PopOver and Bootstrap

--- shop/views.py
+++ shop/views.py
@@ -32,16 +32,16 @@ def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
-    return HttpResponse("We are at contact")
+    return render(request, 'shop/contact.html')
 
 def tracker(request):
-    return HttpResponse("We are at tracker")
+    return render(request, 'shop/tracker.html')
 
 def search(request):
-    return HttpResponse("We are at search")
+    return render(request, 'shop/search.html')
 
 def productView(request):
-    return HttpResponse("We are at productView")
+    return render(request, 'shop/prod_view.html')
 
 def checkout(request):
-    return HttpResponse("We are at checkout")
+    return render(request, 'shop/checkout.html')
