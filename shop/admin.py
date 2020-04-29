1ac9db3438cd1496d52bec2ebe8f005d3a98f4ed
Md. Omar Faruk <iamomar022@gmail.com>
2020-04-29 14:22:11 +0600

    Contact Page and Database

--- shop/admin.py
+++ shop/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Contact
 # Register your models here.
 admin.site.register(Product)
+admin.site.register(Contact)
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from . models import Product
+from . models import Product, Contact
 from math import ceil
 
 # Create your views here.
@@ -32,6 +32,15 @@ def about(request):
     return render(request, 'shop/about.html')
 
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get('name', '')
+        email = request.POST.get('email', '')
+        phone = request.POST.get('phone', '')
+        description = request.POST.get('message', '')
+        
+        contact = Contact(name=name, email=email, phone=phone, description=description)  # First description from db.
+        contact.save()
+
     return render(request, 'shop/contact.html')
 
 def tracker(request):
