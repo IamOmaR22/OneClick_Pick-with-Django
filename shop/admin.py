7e5f4b1f31abafae85e1ee17f058dfe2c1f08bed
Md. Omar Faruk <iamomar022@gmail.com>
2020-05-01 10:45:45 +0600

    Database For Orders

--- shop/admin.py
+++ shop/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Product, Contact
+from .models import Product, Contact, Orders
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Contact)
+admin.site.register(Orders)
