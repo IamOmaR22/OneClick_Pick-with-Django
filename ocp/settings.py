38522c8adc6c267bee46d403a59e95f80e80062d
Md. Omar Faruk <iamomar022@gmail.com>
2020-05-02 11:11:58 +0600

    Home Page For Blog and Shop

--- ocp/settings.py
+++ ocp/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'ocp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['ocp/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
