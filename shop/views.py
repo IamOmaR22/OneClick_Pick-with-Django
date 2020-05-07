97587d39e8c925b1f8c495c846316d580c416f63
Md. Omar Faruk <iamomar022@gmail.com>
2020-05-07 09:44:39 +0600

    Amount Field Added

--- shop/views.py
+++ shop/views.py
@@ -70,13 +70,14 @@ def checkout(request):
     if request.method=="POST":
         items_json = request.POST.get('itemsJson', '')
         name = request.POST.get('name', '')
+        amount = request.POST.get('amount', '')
         email = request.POST.get('email', '')
         address = request.POST.get('address1', '') + " " + request.POST.get('address2', '')
         city = request.POST.get('city', '')
         state = request.POST.get('state', '')
         zip_code = request.POST.get('zip_code', '')
         phone = request.POST.get('phone', '')
-        order = Orders(items_json=items_json, name=name, email=email, address=address, city=city,
+        order = Orders(items_json=items_json, name=name, amount=amount, email=email, address=address, city=city,
                        state=state, zip_code=zip_code, phone=phone)
         order.save()
         update = OrderUpdate(order_id=order.order_id, update_desc="The order has been placed")
