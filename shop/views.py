54ef8cdbb67f8a2bbeb6570b221f08bffb93bfeb
Md. Omar Faruk <iamomar022@gmail.com>
2020-05-08 10:16:06 +0600

    Search Functionality

--- shop/views.py
+++ shop/views.py
@@ -60,8 +60,31 @@ def tracker(request):
     return render(request, 'shop/tracker.html')
 
 
+def searchMatch(query, item):
+    '''return true only if query matches the item'''
+    if query in item.description.lower() or query in item.product_name.lower() or query in item.category.lower():
+        return True
+    else:
+        return False
+
+
 def search(request):
-    return render(request, 'shop/search.html')
+    query = request.GET.get('search')
+    allProds = []
+    catprods = Product.objects.values('category', 'id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prodtemp = Product.objects.filter(category=cat)
+        prod = [item for item in prodtemp if searchMatch(query, item)]
+
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        if len(prod) != 0:
+            allProds.append([prod, range(1, nSlides), nSlides])
+    params = {'allProds': allProds, "msg": ""}
+    if len(allProds) == 0 or len(query)<4:
+        params = {'msg': "Please make sure to enter relevant search query"}
+    return render(request, 'shop/search.html', params)
 
 def productView(request, pid):
     # Fetch the product using the id
